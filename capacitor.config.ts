e129508f50d9351951a3a3344d6552dd879e1ddc
sshaittt1-code <sshaittt1@gmail.com>
2026-03-13 15:35:38 +0200

    Fixed raycast logic to pick first intersected poster

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,16 +4,16 @@ const config: CapacitorConfig = {
   appId: 'com.holocinema.tv',
   appName: 'HoloCinema TV',
   webDir: 'dist',
-  server: {
-    url: 'https://ais-pre-zgturhw4row6gtvlf3jbq3-185322315707.europe-west2.run.app',
-    allowNavigation: [
-      'ais-pre-zgturhw4row6gtvlf3jbq3-185322315707.europe-west2.run.app',
-      '*.run.app',
-      '*.google.com'
-    ],
-    cleartext: true
-  },
-  android: {
+// server: {
+//   url: 'https://ais-pre-zgturhw4row6gtvlf3jbq3-185322315707.europe-west2.run.app',
+//   allowNavigation: [
+//     'ais-pre-zgturhw4row6gtvlf3jbq3-185322315707.europe-west2.run.app',
+//     '*.run.app',
+//     '*.google.com'
+//   ],
+//   cleartext: true
+// },
+android: {
     allowMixedContent: true,
     captureInput: true
   }
